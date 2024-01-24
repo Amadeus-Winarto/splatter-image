c75536f04169e221147b90b4b4f8934c50842b91
hrender <hrender@SOCTF-VR-006>
2024-01-24 10:35:25 +0800

    Fix pretrained discriminator

--- discriminator.py
+++ discriminator.py
@@ -41,25 +41,68 @@ class ResNetBlock(nn.Module):
         out = self.relu2(out)
         return out
 
+class MinibatchStdDev(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        B, _, H, W = x.shape
+        std = x.std(dim=0, keepdim=True).mean()
+        std = std.expand(B, 1, H, W)
+        out = torch.cat([x, std], dim=1)
+        return out
 
 class PretrainedDiscriminator(nn.Module):
-    def __init__(self, model_name="vgg16", in_channel=3, enable_specnorm=False):
+    def __init__(self, model_name="tf_efficientnet_lite0", in_channel=3, enable_specnorm=False):
         super().__init__()
-        if model_name == "vgg16":
+        if model_name == "tf_efficientnet_lite0":
             self.model = timm.create_model(
                 model_name,
                 pretrained=True,
                 in_chans=in_channel,
                 features_only=True,
-                out_indices=(3,),
+                out_indices=(1, 2, 3, 4),
             )
 
-            self.cls = nn.Sequential(
-                ResNetBlock(512, 256, enable_specnorm=enable_specnorm),
-                nn.AdaptiveAvgPool2d(1),
+            self.cls1 = nn.Sequential(
+                ResNetBlock(24, 256, stride=2, enable_specnorm=enable_specnorm),
+                ResNetBlock(256, 512, stride=2, enable_specnorm=enable_specnorm),
+                ResNetBlock(512, 512, stride=2, enable_specnorm=enable_specnorm),
+                ResNetBlock(512, 512, stride=2, enable_specnorm=enable_specnorm),
+                MinibatchStdDev(),
                 nn.Flatten(),
-                nn.Linear(256, 1),
+                nn.Linear(513 * 4 * 4, 512), 
+                nn.LeakyReLU(0.2),
+                nn.Linear(512, 1),
             )
+            self.cls2 = nn.Sequential(
+                ResNetBlock(40, 512, stride=2, enable_specnorm=enable_specnorm),
+                ResNetBlock(512, 512, stride=2, enable_specnorm=enable_specnorm),
+                ResNetBlock(512, 512, stride=2, enable_specnorm=enable_specnorm),
+                MinibatchStdDev(),
+                nn.Flatten(),
+                nn.Linear(513 * 4 * 4, 512), 
+                nn.LeakyReLU(0.2),
+                nn.Linear(512, 1),
+            )
+            self.cls3 = nn.Sequential(
+                ResNetBlock(112, 512, stride=2, enable_specnorm=enable_specnorm),
+                ResNetBlock(512, 512, stride=2, enable_specnorm=enable_specnorm),
+                MinibatchStdDev(),
+                nn.Flatten(),
+                nn.Linear(513 * 4 * 4, 512), 
+                nn.LeakyReLU(0.2),
+                nn.Linear(512, 1),
+            )
+            self.cls4 = nn.Sequential(
+                ResNetBlock(320, 512, stride=2, enable_specnorm=enable_specnorm),
+                MinibatchStdDev(),
+                nn.Flatten(),
+                nn.Linear(513 * 4 * 4, 512), 
+                nn.LeakyReLU(0.2),
+                nn.Linear(512, 1),
+            )
+            self.cls = nn.ModuleList([self.cls1, self.cls2, self.cls3, self.cls4])
         else:
             raise NotImplementedError(f"{model_name} is not implemented")
 
@@ -67,14 +110,23 @@ class PretrainedDiscriminator(nn.Module):
             p.requires_grad_(False)
 
     def extract_features(self, x):
-        x = 0.5 * (x + 1)
         with torch.no_grad():
+            x = nn.functional.interpolate(
+                x,
+                size=(256, 256),
+                mode="bilinear",
+                align_corners=False
+            )
             features = self.model(x)
-        features[0].requires_grad_(True)
-        return features[0]
+        for f in features: 
+            f.requires_grad_(True)
+        return features
 
     def get_logits(self, x):
-        return self.cls(x)
+        logits = []
+        for cls, feature in zip(self.cls, x):
+            logits.append(cls(feature))
+        return logits
 
     def forward(self, x):
         features = self.extract_features(x)
@@ -141,15 +193,23 @@ class Discriminator(nn.Module):
 
 
 if __name__ == "__main__":
-    model = Discriminator()
+    model = PretrainedDiscriminator()
     gamma = 0.2
 
-    x = torch.randn(32, 3, 64, 64)
-    features = model.extract_features(x)
-    logits = model.get_logits(features)
+    x = torch.randn(32, 3, 256, 256)
+    all_features = model.extract_features(x)
+    all_logits = model.get_logits(all_features)
+
+    # Normal loss
+    adv_loss = nn.BCEWithLogitsLoss()
+    loss_d_fake = 0
+    for x in all_logits:
+        loss_d_fake += adv_loss(x, torch.zeros_like(x))
 
     # Feature R1 regularization
-    loss = r1_penalty(logits, [features], gamma=gamma)
+    loss = 0
+    for logits, features in zip(all_logits, all_features):
+        loss += r1_penalty(logits, [features], gamma=gamma)
     loss.backward()
     print("Input shape: ", x.shape)
     print("Features shape: ", features.shape)
